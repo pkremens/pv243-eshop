759f7d12d33a3338ef45104d2eac7547d953fcce
Petr Kremensky <pkremens@redhat.com>
2012-06-12 18:32:32 +0200

    adding products supported, price changet to String, change later back to some num

--- eshop/src/main/resources/import.sql
+++ eshop/src/main/resources/import.sql
@@ -1,8 +1,8 @@
 -- You can use this file to load seed data into the database using SQL statements
 insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212');
 insert into User (email, name, password) values ('abc@abc.cz', 'abc', 'abc');
-insert into Product (id, name, price, visible) values (0, 'stuff', 123, true);
-insert into Product (id, name, price, visible) values (1, 'broken pipe', 1234, true);
-insert into Product (id, name, price, visible) values (2, 'pocket knife', 234, false);
-insert into Product (id, name, price, visible) values (3, 'duck tape', 34, true);
-insert into Product (id, name, price, visible) values (4, 'BFG 9000', 11111134, true);
+insert into Product (id, name, price, visible) values (10, 'stuff', '123', true);
+insert into Product (id, name, price, visible) values (11, 'broken pipe', '1234', true);
+insert into Product (id, name, price, visible) values (12, 'pocket knife', '234', false);
+insert into Product (id, name, price, visible) values (13, 'duck tape', '34', true);
+insert into Product (id, name, price, visible) values (14, 'BFG 9000', '11111134', true);

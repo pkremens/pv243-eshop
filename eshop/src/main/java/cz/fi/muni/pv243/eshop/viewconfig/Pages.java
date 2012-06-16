c3aa9cd98ec4d944a4599dba6bbb8a0c54208254
Tomas Vlcek <tomas.vlcoun@gmail.com>
2012-06-16 19:58:54 +0200

    So many changes. New design for all pages, add seller page, ...

--- eshop/src/main/java/cz/fi/muni/pv243/eshop/viewconfig/Pages.java
+++ eshop/src/main/java/cz/fi/muni/pv243/eshop/viewconfig/Pages.java
@@ -9,6 +9,7 @@ import org.jboss.seam.faces.view.config.ViewPattern;
 import org.jboss.seam.security.annotations.LoggedIn;
 
 import cz.fi.muni.pv243.eshop.service.Admin;
+import cz.fi.muni.pv243.eshop.service.Seller;
 
 
 @ViewConfig
@@ -33,6 +34,14 @@ public interface Pages {
 		@Admin
 		@RestrictAtPhase({PhaseIdType.RESTORE_VIEW, PhaseIdType.INVOKE_APPLICATION})
 		ADMIN_PAGE,
+		
+		@ViewPattern("/seller_page.xhtml")
+		@LoginView("/login.xhtml")
+		@AccessDeniedView("/denied.xhtml")
+		@LoggedIn
+		@Seller
+		@RestrictAtPhase({PhaseIdType.RESTORE_VIEW, PhaseIdType.INVOKE_APPLICATION})
+		SELLER_PAGE,
 		
 		@ViewPattern("/orders.xhtml")
 		@LoginView("/login.xhtml")

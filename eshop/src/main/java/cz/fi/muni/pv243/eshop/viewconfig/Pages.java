3dc809b18caa8e5192aa635a452b58dbc9e0a997
MatousJobanek <matousjobanek@gmail.com>
2012-06-16 20:05:43 +0200

    preparation fo test-arquillian

--- eshop/src/main/java/cz/fi/muni/pv243/eshop/viewconfig/Pages.java
+++ eshop/src/main/java/cz/fi/muni/pv243/eshop/viewconfig/Pages.java
@@ -1,68 +1,70 @@
 package cz.fi.muni.pv243.eshop.viewconfig;
 
-import org.jboss.seam.faces.event.PhaseIdType;
-import org.jboss.seam.faces.security.AccessDeniedView;
-import org.jboss.seam.faces.security.LoginView;
-import org.jboss.seam.faces.security.RestrictAtPhase;
-import org.jboss.seam.faces.view.config.ViewConfig;
-import org.jboss.seam.faces.view.config.ViewPattern;
-import org.jboss.seam.security.annotations.LoggedIn;
-
-import cz.fi.muni.pv243.eshop.service.Admin;
-
-
-@ViewConfig
+//@ViewConfig
 public interface Pages {
 	static enum Pages1 {
-		
-		@ViewPattern("/index.xhtml")
-        INDEX,
-		
-        @ViewPattern("/tables.xhtml")
-		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
-		@LoggedIn
-		@Admin
-		@RestrictAtPhase({PhaseIdType.RESTORE_VIEW, PhaseIdType.INVOKE_APPLICATION})
-		TABLES,
-		
-		@ViewPattern("/admin_page.xhtml")
-		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
-		@LoggedIn
-		@Admin
-		@RestrictAtPhase({PhaseIdType.RESTORE_VIEW, PhaseIdType.INVOKE_APPLICATION})
-		ADMIN_PAGE,
-		
-		@ViewPattern("/test.xhtml")
-		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
-		@LoggedIn
-		@RestrictAtPhase({PhaseIdType.RESTORE_VIEW, PhaseIdType.INVOKE_APPLICATION})
-		TEST,
-		
+
+		// @ViewPattern("/index.xhtml")
+		// INDEX,
+		//
+		// @ViewPattern("/tables.xhtml")
+		// @LoginView("/login.xhtml")
+		// @AccessDeniedView("/denied.xhtml")
+		// @LoggedIn
+		// @Admin
+		// @RestrictAtPhase({PhaseIdType.RESTORE_VIEW,
+		// PhaseIdType.INVOKE_APPLICATION})
+		// TABLES,
+		//
+		// @ViewPattern("/admin_page.xhtml")
+		// @LoginView("/login.xhtml")
+		// @AccessDeniedView("/denied.xhtml")
+		// @LoggedIn
+		// @Admin
+		// @RestrictAtPhase({PhaseIdType.RESTORE_VIEW,
+		// PhaseIdType.INVOKE_APPLICATION})
+		// ADMIN_PAGE,
+		//
+		// @ViewPattern("/test.xhtml")
+		// @LoginView("/login.xhtml")
+		// @AccessDeniedView("/denied.xhtml")
+		// @LoggedIn
+		// @RestrictAtPhase({PhaseIdType.RESTORE_VIEW,
+		// PhaseIdType.INVOKE_APPLICATION})
+		// TEST,
+
 		/*
-		@ViewPattern("/view.xhtml")
-		@UrlMapping(pattern="/view/#{id}/")
-        VIEW,
-        
-        @ViewPattern("/edit.xhtml")
-		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
-		@LoggedIn
-		@IsManagerOf
-		@RestrictAtPhase({PhaseIdType.RESTORE_VIEW, PhaseIdType.INVOKE_APPLICATION})
-        EDIT,
-        
-        @ViewPattern("/bakerylist.xhtml")
-        BAKERYLIST,
-        
-        @ViewPattern("/managers.xhtml")
-		@LoginView("/login.xhtml")
-        @AccessDeniedView("/denied.xhtml")
-		@LoggedIn
-		@Admin
-		@RestrictAtPhase({PhaseIdType.RESTORE_VIEW, PhaseIdType.INVOKE_APPLICATION})
-		MANAGERS*/
-    }
+		 * @ViewPattern("/view.xhtml")
+		 * 
+		 * @UrlMapping(pattern="/view/#{id}/") VIEW,
+		 * 
+		 * @ViewPattern("/edit.xhtml")
+		 * 
+		 * @LoginView("/login.xhtml")
+		 * 
+		 * @AccessDeniedView("/denied.xhtml")
+		 * 
+		 * @LoggedIn
+		 * 
+		 * @IsManagerOf
+		 * 
+		 * @RestrictAtPhase({PhaseIdType.RESTORE_VIEW,
+		 * PhaseIdType.INVOKE_APPLICATION}) EDIT,
+		 * 
+		 * @ViewPattern("/bakerylist.xhtml") BAKERYLIST,
+		 * 
+		 * @ViewPattern("/managers.xhtml")
+		 * 
+		 * @LoginView("/login.xhtml")
+		 * 
+		 * @AccessDeniedView("/denied.xhtml")
+		 * 
+		 * @LoggedIn
+		 * 
+		 * @Admin
+		 * 
+		 * @RestrictAtPhase({PhaseIdType.RESTORE_VIEW,
+		 * PhaseIdType.INVOKE_APPLICATION}) MANAGERS
+		 */
+	}
 }
--- eshop/src/test/java/cz/fi/muni/pv243/eshop/test/CustomerControllerTest.java
+++ eshop/src/test/java/cz/fi/muni/pv243/eshop/test/CustomerControllerTest.java
@@ -5,7 +5,6 @@ package cz.fi.muni.pv243.eshop.test;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.File;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
@@ -14,7 +13,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -31,17 +30,20 @@ import cz.fi.muni.pv243.eshop.service.CustomerManagerImpl;
 public class CustomerControllerTest {
 
 	@Deployment
-	public static WebArchive createTestArchive() {
-		System.err.println("test");
-		return ShrinkWrap
-				.create(WebArchive.class, "test.war")
+	public static JavaArchive createDeployment() {
+		System.err.println("set test");
+		JavaArchive webArch = ShrinkWrap
+				.create(JavaArchive.class)
 				.addClasses(CustomerBean.class, CustomerManager.class,
 						CustomerManagerImpl.class, Customer.class)
-				.addAsResource("META-INF/persistence.xml")
-				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-				.setWebXML(new File("WEB-INF/web.xml"))
-				.addAsWebInfResource(new File("WEB-INF/faces-config.xml"))
-				.addAsWebInfResource(new File("WEB-INF/eshop-ds.xml"));
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+		// .addAsResource("META-INF/persistence.xml");
+		// .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+		// .setWebXML(new File("WEB-INF/web.xml"));
+		// .addAsWebInfResource(new File("WEB-INF/faces-config.xml"));
+		// .addAsWebInfResource(new File("WEB-INF/eshop-ds.xml"));
+		System.err.println(webArch.toString(true));
+		return webArch;
 
 	}
 
@@ -53,7 +55,7 @@ public class CustomerControllerTest {
 
 	@Test
 	public void testRegister() throws Exception {
-		System.err.println("test");
+		System.err.println("run test");
 		customerController.initNewCustomer();
 		customerController.getNewCustomer().setName("Jane Doe");
 		customerController.getNewCustomer().setEmail("jane@mailinator.com");
